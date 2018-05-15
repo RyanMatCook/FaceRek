4e166ccafec3492fb59f70d385e07a342118c3a1
Ryan Cook <mr.rmcook@gmail.com>
2018-05-15 07:40:30 +0200

    Add project files.

---
+++ FaceRek/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace FaceRek
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.imbxCurrentFrame = new Emgu.CV.UI.ImageBox();
+			((System.ComponentModel.ISupportInitialize)(this.imbxCurrentFrame)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// imbxCurrentFrame
+			// 
+			this.imbxCurrentFrame.Location = new System.Drawing.Point(169, 75);
+			this.imbxCurrentFrame.Name = "imbxCurrentFrame";
+			this.imbxCurrentFrame.Size = new System.Drawing.Size(414, 282);
+			this.imbxCurrentFrame.TabIndex = 2;
+			this.imbxCurrentFrame.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.imbxCurrentFrame);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.imbxCurrentFrame)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Emgu.CV.UI.ImageBox imbxCurrentFrame;
+	}
+}
+
---
+++ FaceRek/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Cvb;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace FaceRek
+{
+	public partial class Form1 : Form
+	{
+
+		private static VideoCapture _cameraCapture;
+
+		private static IBackgroundSubtractor _fgDetector;
+		private static Emgu.CV.Cvb.CvBlobDetector _blobDetector;
+		private static Emgu.CV.Cvb.CvTracks _tracker;
+
+		public Form1()
+		{
+			InitializeComponent();
+			Run();
+		}
+
+		void Run()
+		{
+			try
+			{
+				_cameraCapture = new VideoCapture();
+				//_cameraCapture.Width = 600;
+				//_cameraCapture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+				//_cameraCapture.set(CV_CAP_PROP_FOURCC, CV_FOURCC('B', 'G', 'R', '3'));
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message);
+				return;
+			}
+
+			//_fgDetector = new BackgroundSubtractorMOG2();
+			//_blobDetector = new CvBlobDetector();
+			//_tracker = new CvTracks();
+
+			Application.Idle += ProcessFrame;
+		}
+
+		void ProcessFrame(object sender, EventArgs e)
+		{
+			_cameraCapture.QueryFrame();
+			Mat frame = _cameraCapture.QueryFrame();
+
+			List<Rectangle> faces = new List<Rectangle>();
+			List<Rectangle> eyes = new List<Rectangle>();
+			DetectFace.Detect(frame, faces, eyes, out var detectionTime);
+			detectionTime++;
+
+			foreach (Rectangle face in faces)
+				CvInvoke.Rectangle(frame, face, new Bgr(Color.Red).MCvScalar, 2);
+			foreach (Rectangle eye in eyes)
+				CvInvoke.Rectangle(frame, eye, new Bgr(Color.Blue).MCvScalar, 2);
+			//Mat smoothedFrame = new Mat();
+			//CvInvoke.GaussianBlur(frame, smoothedFrame, new Size(3, 3), 1); //filter out noises
+			////frame._SmoothGaussian(3); 
+
+			//#region use the BG/FG detector to find the forground mask
+			//Mat forgroundMask = new Mat();
+			//_fgDetector.Apply(smoothedFrame, forgroundMask);
+			//#endregion
+
+			//CvBlobs blobs = new CvBlobs();
+			//_blobDetector.Detect(forgroundMask.ToImage<Gray, byte>(), blobs);
+			//blobs.FilterByArea(100, int.MaxValue);
+
+			//float scale = (frame.Width + frame.Width) / 2.0f;
+			//_tracker.Update(blobs, 0.01 * scale, 5, 5);
+
+			//foreach (var pair in _tracker)
+			//{
+			//	CvTrack b = pair.Value;
+			//	CvInvoke.Rectangle(frame, b.BoundingBox, new MCvScalar(255.0, 255.0, 255.0), 2);
+			//	CvInvoke.PutText(frame, b.Id.ToString(), new Point((int)Math.Round(b.Centroid.X), (int)Math.Round(b.Centroid.Y)), FontFace.HersheyPlain, 1.0, new MCvScalar(255.0, 255.0, 255.0));
+			//}
+			imbxCurrentFrame.Image = frame.ToImage<>();
+		}
+	}
+}
